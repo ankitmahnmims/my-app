c0def8188867d0a5b2a2fa8e670083d4410150f2
ankitmahnmims <ankitmah.nmims@gmail.com>
2024-05-02 20:34:21 +0530

    update js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
     extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier'],
     plugins: ['react', 'react-hooks', 'prettier'],
+    parser: "babel-eslint",
     rules: {
       'prettier/prettier': 'error'
     },
+      "parserOptions": {
+          "sourceType": "module"
+      },
     settings: {
       react: {
         version: 'detect'

07a4ee04e9a808b7903e79ba9b2ba6a6f4aff209
ankitmahnmims <ankitmah.nmims@gmail.com>
2024-05-02 20:59:09 +0530

    updating js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
     extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier'],
     plugins: ['react', 'react-hooks', 'prettier'],
     rules: {
-      'prettier/prettier': 'error'
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
     },
     settings: {
       react: {

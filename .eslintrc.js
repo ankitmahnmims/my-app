947d87be6a366ce4b30e3f7f6538e6344f977a21
ankitmahnmims <ankitmah.nmims@gmail.com>
2024-05-02 20:48:42 +0530

    update jsss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
     parserOptions: {
-      "sourceType": "module",
-      "ecmaVersion": 2020
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true // Enable JSX
+    }
   },
     extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier'],
     plugins: ['react', 'react-hooks', 'prettier'],

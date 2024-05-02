f6d5a26c553253f49677d9745322c943a56cb1ba
ankitmahnmims <ankitmah.nmims@gmail.com>
2024-05-02 18:56:10 +0530

    updating the code

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'prettier'],
+    plugins: ['react', 'react-hooks', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  };
+  
